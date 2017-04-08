428d00eaa4aafa2df9a1bfe61629d89a97dfcb3b
Dexter Lewis <dexterlewis@gmail.com>
2017-04-08 15:42:31 -0400

    setup database connection and port

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
 /**
  * Created by Admin on 4/8/2017.
  */
+exportsDATABASE_URL = process.env.DATABASE_URL ||
+            global.DATABASE_URL ||
+            'mongodb:localhost/blobDB';
+
+exports.PORT = process.env.PORT || 8080;
